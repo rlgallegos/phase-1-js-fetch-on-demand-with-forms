6c0bad7ceaa72e5c7675a78f88fe81fcb5866705
Robert <rlgallegos85@gmail.com>
2023-02-04 13:05:10 -0500

    fetch on demand with forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
-  
-}
+
+    const userForm = document.body.querySelector('form');
+
+
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        //extract value
+        let input = document.getElementById('searchByID').value;
+        
+        fetch(`http://localhost:3000/movies/${input}`)
+            .then(response => response.json())
+            .then(data => {
+                const title = document.querySelector('#movieDetails h4')
+                const summary = document.querySelector('#movieDetails p')
+                
+                if (data.title) {
+                title.innerText = data.title;
+                summary.innerText = data.summary
+                }
+                else {
+                    title.innerText = "ID not found"
+                    summary.innerText = ""
+                }
+            });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
